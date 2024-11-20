5954f335300397ddda59ee340656b39b327280c0
Zach Leatherman <zachleat@users.noreply.github.com>
2024-11-20 11:49:52 -0600

    Use proper inheritance for BlueskyUser type to Rss

--- src/DataSource/BlueskyUser.js
+++ src/DataSource/BlueskyUser.js
@@ -1,14 +1,14 @@
 import path from "node:path";
-import { DataSource } from "../DataSource.js";
+import { Rss } from "./Rss.js";
 
-class BlueskyUser extends DataSource {
+class BlueskyUser extends Rss {
 	static TYPE = "bluesky";
 	static TYPE_FRIENDLY = "Bluesky";
 
 	#username;
 
 	constructor(username) {
-		super();
+		super(`https://bsky.app/profile/${username}/rss`);
 		this.username = username;
 	}
 
@@ -24,30 +24,6 @@ class BlueskyUser extends DataSource {
 		return this.#username;
 	}
 
-	getType() {
-		return "xml";
-	}
-
-	getUrl() {
-		return `https://bsky.app/profile/${this.username}/rss`
-	}
-
-	getEntriesFromData(data) {
-		if(Array.isArray(data.rss?.channel?.item)) {
-			return data.rss.channel.item;
-		}
-
-		if(data.rss?.channel?.item) {
-			return [data.rss.channel.item];
-		}
-
-		return [];
-	}
-
-	getUniqueIdFromEntry(entry) {
-		return `${DataSource.UUID_PREFIX}::${BlueskyUser.TYPE}::${entry.link}`;
-	}
-
 	static getFilePath(url) {
 		let {pathname} = new URL(url);
 		let [empty, profile, username, post, id] = pathname.split("/");
@@ -55,21 +31,11 @@ class BlueskyUser extends DataSource {
 	}
 
 	cleanEntry(entry, data) {
-		return {
-			uuid: this.getUniqueIdFromEntry(entry),
-			type: BlueskyUser.TYPE,
-			title: this.toReadableDate(entry.pubDate),
-			url: entry.link,
-			authors: [
-				{
-					name: data.rss.channel.title,
-					url: data.rss.channel.link,
-				}
-			],
-			date: this.toIsoDate(entry.pubDate),
-			content: entry.description,
-			contentType: "text",
-		}
+		let obj = super.cleanEntry(entry, data);
+		obj.type = BlueskyUser.TYPE;
+		obj.contentType = "text";
+
+		return obj;
 	}
 }
 
